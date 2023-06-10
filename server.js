dc245b924e6785b0ad1dc70686b8351be6694ca8
“[Maulana <[maulana.nurhendronoto@ui.ac.id]>
2023-06-10 09:09:45 +0700

    add front end, project complete

--- server.js
+++ server.js
@@ -1,29 +1,39 @@
 const express = require('express');
-const bodyParser = require('body-parser');
-const fetch = require('node-fetch');
 const app = express();
+const fetch = require('node-fetch');
+const bodyParser = require('body-parser');
 
-app.use(bodyParser.urlencoded({extended: true}));
 require('dotenv').config();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
+    let locDate = {temp: "Temp", desc: "Description", location: "Location", humidity: "Humidity", feel: "Feel", speed: "Speed"};
+    res.render('index', {locDate: locDate});
 });
 
-app.post("/", async(req, res) => {
-    let location = await req.body.city;
-    const url = `http://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${process.env.APIKEY}&units=metric`;
-    const response = await fetch(url);
-    const weatherData = await response.json();
-    const temp = Math.floor(weatherData.main.temp);
-    const weatherDescription = weatherData.weather[0].description;
-    const icon = weatherData.weather[0].icon;
-    const imageUrl = `https://openweathermap.org/img/wn/${icon}@2x.png`;
-    res.write(`<h1>The current weather in ${location} is ${weatherDescription}</h1>`);
-    res.write(`<h1>The current temperature is ${temp} degree Celsius</h1>`);
-    res.write(`<img src=${imageUrl}>`);
+app.post('/', async (req, res) => {
+    try{
+        const location = await req.body.city;
+        const url = `http://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${process.env.APIKEY}&units=metric`;
+        let response = await fetch(url);
+        let data = await response.json();
+        let locDate = {};
+        locDate.temp = Math.floor(data.main.temp);
+        locDate.desc = data.weather[0].description;
+        locDate.feel = Math.floor(data.main.feels_like);
+        locDate.humidity = data.main.humidity;
+        locDate.speed = data.wind.speed;
+        locDate.location = location;
+        console.log(locDate);
+        res.render('index', {locDate: locDate});
+    } catch(err){
+        console.log(err);
+        res.status(400).json({data: 'not found'})
+    }
 });
 
 app.listen(3000, () => {
-    console.log('Server is running on port 3000.');
+    console.log('Server is running on port 3000');
 });
